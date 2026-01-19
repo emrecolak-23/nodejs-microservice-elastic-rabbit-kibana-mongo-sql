46f00f69116c5b2aa92ab3f5bd0b650e25bc1509
mjoe <colakkemre@gmail.com>
2026-01-19 23:43:45 +0300

    feat(notification-service): add health check route and configuration for RabbitMQ and Elasticsearch

---
+++ server/notification-service/src/server.ts
@@ -0,0 +1,33 @@
+import { winstonLogger } from '@emrecolak-23/jobber-share';
+import { Logger } from 'winston';
+import { config } from '@notifications/config';
+import { Application } from 'express';
+import http from 'http';
+import { healthRoutes } from './route';
+
+const SERVER_PORT = 4001;
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'notificationServer', 'debug');
+
+export function start(app: Application): void {
+  startServer(app);
+  // http://localhost:4001/notification-health
+  app.use('', healthRoutes);
+  startsQueues();
+  startElasticSearch();
+}
+
+async function startsQueues(): Promise<void> {}
+
+function startElasticSearch(): void {}
+
+function startServer(app: Application): void {
+  try {
+    const httpServer: http.Server = new http.Server(app);
+    log.info(`Worker with process id of ${process.pid} on notification server has started`);
+    httpServer.listen(SERVER_PORT, () => {
+      log.info(`Notification server running on port ${SERVER_PORT}`);
+    });
+  } catch (error) {
+    log.log(`error`, 'NotificationService startServer() method: ', error);
+  }
+}
