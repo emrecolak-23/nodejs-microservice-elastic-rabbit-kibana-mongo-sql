c1c7d421cc7c2226eec59282684a15772e40d8e2
mjoe <colakkemre@gmail.com>
2026-01-25 01:22:44 +0300

    feat(gateway-service): implement startServer and startHttpServer methods for server initialization

--- server/gateway-service/src/server.ts
+++ server/gateway-service/src/server.ts
@@ -7,10 +7,11 @@ import helmet from 'helmet';
 import cors from 'cors';
 import compression from 'compression';
 import StatusCodes from 'http-status-codes';
-import { CustomError, IErrorResponse } from '@emrecolak-23/jobber-share/src/error-handler';
+import http from 'http';
+import { CustomError, IErrorResponse } from '@emrecolak-23/jobber-share';
 
 const SERVER_PORT = process.env.SERVER_PORT || 4000;
-const log: Logger = winstonLogger('', 'apiGatewayServer', 'debug');
+const log: Logger = winstonLogger('http://localhost:9200', 'apiGatewayServer', 'debug');
 
 export class GatewayServer {
   constructor(private readonly app: Application) {}
@@ -21,6 +22,7 @@ export class GatewayServer {
     this.routesMiddleware(this.app);
     this.startsElasticSearch();
     this.errorHandler();
+    this.startServer(this.app);
   }
 
   private securityMiddleware(app: Application): void {
@@ -73,5 +75,25 @@ export class GatewayServer {
       res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: `The endpoint called does not exist` });
       next();
     });
+  }
+
+  private async startServer(app: Application): Promise<void> {
+    try {
+      const httpServer: http.Server = new http.Server(app);
+      await this.startHttpServer(httpServer);
+    } catch (err) {
+      log.log('error', 'GatewayService startServer() error method: ', err);
+    }
+  }
+
+  private async startHttpServer(httpServer: http.Server): Promise<void> {
+    try {
+      log.info(`Geteay server has started with process id of ${process.pid} on. gateway server has started`);
+      httpServer.listen(SERVER_PORT, () => {
+        log.info(`Gateway server running on port ${SERVER_PORT}`);
+      });
+    } catch (err) {
+      log.log('error', 'GatewayService startHttpServer() error method: ', err);
+    }
   }
 }
