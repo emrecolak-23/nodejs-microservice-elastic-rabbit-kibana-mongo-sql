e4132559ac1a7f31b6ec52d87a3566508d131ea5
mjoe <colakkemre@gmail.com>
2026-02-02 22:47:09 +0300

    feat(auth-service): enhance AuthModel with password hashing, comparison methods, and unique email/username constraints

--- server/auth-service/src/models/auth.schema.ts
+++ server/auth-service/src/models/auth.schema.ts
@@ -1,55 +1,108 @@
-import { sequelize } from "@auth/loaders";
-import { IAuthDocument } from "@emrecolak-23/jobber-share";
-import { DataTypes, ModelDefined, Optional } from "sequelize";
+import { sequelize } from '@auth/loaders';
+import { IAuthDocument } from '@emrecolak-23/jobber-share';
+import { compare, hash } from 'bcryptjs';
+import { DataTypes, Model, ModelDefined, Optional } from 'sequelize';
+
+const SALT_ROUND = 10;
+
+interface AuthModelInstanceMethods extends Model {
+  prototype: {
+    comparePassword(password: string, hashedPassword: string): Promise<boolean>;
+    hashPassword(password: string): Promise<string>;
+  };
+}
 
 type AuthUserCreationAttributes = Optional<IAuthDocument, 'id' | 'createdAt' | 'passwordResetToken' | 'passwordResetExpires'>;
 
-const AuthModel: ModelDefined<IAuthDocument, AuthUserCreationAttributes> = sequelize.define("auths", {
+const AuthModel: ModelDefined<IAuthDocument, AuthUserCreationAttributes> & AuthModelInstanceMethods = sequelize.define(
+  'auths',
+  {
     username: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     profilePublicId: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     email: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     country: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     profilePicture: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false
     },
     emailVerificationToken: {
-        type: DataTypes.STRING,
-        allowNull: true
+      type: DataTypes.STRING,
+      allowNull: true
     },
     emailVerified: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: 0
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: 0
     },
     createdAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
     },
     updatedAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
     },
     passwordResetToken: {
-        type: DataTypes.STRING,
-        allowNull: true
+      type: DataTypes.STRING,
+      allowNull: true
     },
-})
+    passwordResetExpires: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: new Date()
+    }
+  },
+  {
+    indexes: [
+      {
+        unique: true,
+        fields: ['email']
+      },
+      {
+        unique: true,
+        fields: ['username']
+      }
+    ]
+  }
+) as ModelDefined<IAuthDocument, AuthUserCreationAttributes> & AuthModelInstanceMethods;
+
+AuthModel.addHook('beforeCreate', async (auth: any) => {
+  if (auth.dataValues.password) {
+    auth.dataValues.password = await hash(auth.dataValues.password as string, SALT_ROUND);
+  }
+});
+
+AuthModel.addHook('beforeCreate', async (auth: Model) => {
+  const hashedPassword: string = await hash(auth.dataValues.password as string, SALT_ROUND);
+  auth.dataValues.password = hashedPassword;
+});
+
+AuthModel.prototype.comparePassword = async function (password: string, hashedPassword: string): Promise<boolean> {
+  return compare(password, hashedPassword);
+};
+
+AuthModel.prototype.hashPassword = async function (password: string): Promise<string> {
+  return hash(password, SALT_ROUND);
+};
+
+// force: treu always deletes tha table where there is a server restart
+AuthModel.sync({});
+export { AuthModel };
