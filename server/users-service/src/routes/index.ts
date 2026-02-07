63add177d43ced023ff5ba48ed4347c0d083dfe2
mjoe <colakkemre@gmail.com>
2026-02-07 21:29:57 +0300

    feat(users-service): integrate seller routes into the application for improved seller management

--- server/users-service/src/routes/index.ts
+++ server/users-service/src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Application } from 'express';
 import { HealthRoute } from '@users/routes/health.routes';
 import { BuyerRoute } from '@users/routes/buyer.routes';
+import { SellerRoute } from '@users/routes/seller.routes';
 import { container } from 'tsyringe';
 import { verifyGatewayRequest } from '@emrecolak-23/jobber-share';
 
@@ -10,8 +11,9 @@ const SELLER_BASE_PATH = '/api/v1/seller';
 export const appRoutes = (app: Application) => {
   const healthRoutes = container.resolve(HealthRoute);
   const buyerRoutes = container.resolve(BuyerRoute);
-  
+  const sellerRoutes = container.resolve(SellerRoute);
+
   app.use('', healthRoutes.routes());
   app.use(BUYER_BASE_PATH, verifyGatewayRequest, buyerRoutes.routes());
-  app.use(SELLER_BASE_PATH, verifyGatewayRequest, () => console.log('Seller routes'));
+  app.use(SELLER_BASE_PATH, verifyGatewayRequest, sellerRoutes.routes());
 };
