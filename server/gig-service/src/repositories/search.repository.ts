83d3ca80e796df5fcefd2e90acca024546e27e9a
mjoe <colakkemre@gmail.com>
2026-02-09 12:25:00 +0300

    feat(gig-service): implement advanced search methods in SearchRepository for gig retrieval and filtering

--- server/gig-service/src/repositories/search.repository.ts
+++ server/gig-service/src/repositories/search.repository.ts
@@ -1,6 +1,6 @@
 import { injectable, singleton } from 'tsyringe';
 import { ElasticSearch } from '@gig/loaders';
-import { IHitsTotal, IQueryList, ISearchResult, winstonLogger } from '@emrecolak-23/jobber-share';
+import { IHitsTotal, IPaginateProps, IQueryList, ISearchResult, winstonLogger } from '@emrecolak-23/jobber-share';
 
 import { SearchResponse } from '@elastic/elasticsearch/lib/api/types';
 import { Logger } from 'winston';
@@ -51,5 +51,161 @@ export class SearchRepository {
 
   async getGigCount(): Promise<number> {
     return await this.elasticSearch.getDocumentCount('gigs');
+  }
+
+  async gigsSearch(
+    searchQuery: string,
+    paginate: IPaginateProps,
+    deliveryTime?: string,
+    min?: number,
+    max?: number
+  ): Promise<ISearchResult> {
+    const { from, size, type } = paginate;
+
+    const queryList: IQueryList[] = [
+      {
+        query_string: {
+          fields: ['username', 'title', 'description', 'basicDescription', 'basicTitle', 'categories', 'subCategories', 'tags'],
+          query: `*${searchQuery}*`
+        }
+      },
+      {
+        term: {
+          active: true
+        }
+      }
+    ];
+
+    if (deliveryTime !== undefined && deliveryTime !== '') {
+      queryList.push({
+        query_string: {
+          fields: ['expectedDelivery'],
+          query: `*${deliveryTime}*`
+        }
+      });
+    }
+
+    if (!isNaN(parseInt(`${min}`)) && !isNaN(parseInt(`${max}`))) {
+      queryList.push({
+        range: {
+          price: {
+            gte: min,
+            lte: max
+          }
+        }
+      });
+    }
+
+    this.log.info(`Search Query in search repository: ${JSON.stringify(queryList)}`);
+    this.log.info(`Search Params in search repository: ${JSON.stringify({ searchQuery, from, size, type })}`);
+
+    const result: SearchResponse = await this.elasticSearch.elasticSearchClient.search({
+      index: 'gigs',
+      size,
+      query: {
+        bool: {
+          must: queryList as any
+        }
+      },
+      sort: [
+        {
+          sortId: type === 'forward' ? 'asc' : 'desc'
+        }
+      ],
+      ...(from !== '0' && { search_after: [from] })
+    });
+    const total: IHitsTotal = result.hits.total as IHitsTotal;
+    return {
+      total: total.value,
+      hits: result.hits.hits
+    };
+  }
+
+  async gigsSearchByCategory(searchQuery: string): Promise<ISearchResult> {
+    const result: SearchResponse = await this.elasticSearch.elasticSearchClient.search({
+      index: 'gigs',
+      size: 10,
+      query: {
+        bool: {
+          must: [
+            {
+              query_string: {
+                fields: ['categories'],
+                query: `*${searchQuery}*`
+              }
+            },
+            {
+              term: {
+                active: true
+              }
+            }
+          ] as any
+        }
+      }
+    });
+    const total: IHitsTotal = result.hits.total as IHitsTotal;
+    return {
+      total: total.value,
+      hits: result.hits.hits
+    };
+  }
+
+  async getMoreGigsLikeThis(gigId: string): Promise<ISearchResult> {
+    const result: SearchResponse = await this.elasticSearch.elasticSearchClient.search({
+      index: 'gigs',
+      size: 5,
+      query: {
+        more_like_this: {
+          fields: ['username', 'title', 'description', 'basicDescription', 'basicTitle', 'categories', 'subCategories', 'tags'],
+          like: [
+            {
+              _index: 'gigs',
+              _id: gigId
+            }
+          ]
+        }
+      }
+    });
+
+    const total: IHitsTotal = result.hits.total as IHitsTotal;
+    return {
+      total: total.value,
+      hits: result.hits.hits
+    };
+  }
+
+  async getTopRatedGigsByCategory(searchQuery: string): Promise<ISearchResult> {
+    const result: SearchResponse = await this.elasticSearch.elasticSearchClient.search({
+      index: 'gigs',
+      size: 10,
+      query: {
+        bool: {
+          filter: {
+            script: {
+              script: {
+                source: "doc['ratingSum'].value != 0 && (doc['ratingSum'].value / doc['ratingsCount'].value == params.threshold)",
+                lang: 'painless',
+                params: {
+                  threshold: 5
+                }
+              }
+            }
+          },
+          must: [
+            {
+              query_string: {
+                fields: ['categories'],
+                query: `*${searchQuery}*`
+              }
+            }
+          ]
+        }
+      }
+    });
+    const total: IHitsTotal = result.hits.total as IHitsTotal;
+    return {
+      total: total.value,
+      hits: result.hits.hits
+    };
   }
 }
