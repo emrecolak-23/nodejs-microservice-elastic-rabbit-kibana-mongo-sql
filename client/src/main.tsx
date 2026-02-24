3f414bc364ab7441033b425c536444c29a67b9be
mjoe <colakkemre@gmail.com>
2026-02-25 00:26:14 +0300

    feat(store): integrate redux-persist for state management and configure store with middleware; update main entry point to include Provider and PersistGate

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App.tsx';
+import { Persistor } from 'redux-persist';
+import { persistStore } from 'redux-persist';
+import { store } from './store/store.ts';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistor: Persistor = persistStore(store);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </StrictMode>
 );
--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,11 +1,10 @@
 import storage from 'redux-persist/lib/storage';
-import { combineReducers } from '@reduxjs/toolkit';
+import { combineReducers, configureStore, EnhancedStore } from '@reduxjs/toolkit';
 import { AnyAction, Reducer } from 'redux';
-import { persistReducer } from 'redux-persist';
+import { FLUSH, PAUSE, PERSIST, persistReducer, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 export const combineReducer = combineReducers({});
-
-export type RootState = ReturnType<typeof combineReducer>;
 
 export const rootReducer: Reducer<RootState, AnyAction> = (state, action) => {
   // this is to reset the state to default when user logout
@@ -23,3 +22,20 @@ const persistConfig = {
 };
 
 export const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store: EnhancedStore = configureStore({
+  reducer: persistedReducer,
+  devTools: true,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
