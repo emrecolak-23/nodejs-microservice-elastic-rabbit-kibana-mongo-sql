dfc28db713eb55ef6125be180b414fd354c372cd
mjoe <colakkemre@gmail.com>
2026-02-25 15:39:13 +0300

    feat(Login): implement user login functionality with validation, alert handling, and loading state management

--- client/src/features/auth/components/Login.tsx
+++ client/src/features/auth/components/Login.tsx
@@ -1,12 +1,50 @@
-import { FC, ReactElement } from 'react';
+import { ChangeEvent, FC, ReactElement, useState } from 'react';
 import { IModalBgProps } from 'src/shared/modals/interfaces/modal.interface';
 import Button from 'src/shared/button/Button';
 import Alert from 'src/shared/alert/Alert';
 import TextInput from 'src/shared/inputs/TextInput';
-import { FaEye, FaTimes } from 'react-icons/fa';
+import { FaEye, FaEyeSlash, FaTimes } from 'react-icons/fa';
 import ModalBg from 'src/shared/modals/modalBg';
+import { ISignInPayload } from '../interfaces/auth.interface';
+import { useAppDispatch } from 'src/store/store';
+import { useAuthSchema } from '../hooks/useAuthSchema';
+import { loginUserSchema } from '../schemes/auth.schema';
+import { useSignInMutation } from '../services/auth.service';
+import { IResponse } from 'src/shared/shared.interface';
+import { addAuthUser } from '../reducers/auth.reducer';
+import { updateLogout } from '../reducers/logout.reducer';
+import { saveToSessionStorage } from 'src/shared/utils/utils.service';
+import { cn } from 'src/shared/utils/cn';
 
 const Login: FC<IModalBgProps> = ({ onClose, onToggle, onTogglePassword }): ReactElement => {
+  const [alertMessage, setAlertMessage] = useState<string>('');
+  const [passwordType, setPasswordType] = useState<string>('password');
+  const [userInfo, setUserInfo] = useState<ISignInPayload>({
+    username: '',
+    password: ''
+  } as ISignInPayload);
+
+  const dispatch = useAppDispatch();
+  const [schemaValidation] = useAuthSchema({ schema: loginUserSchema, userInfo });
+  const [signIn, { isLoading }] = useSignInMutation();
+
+  const onLoginUser = async (): Promise<void> => {
+    try {
+      const isValid = await schemaValidation();
+      if (isValid) {
+        const result: IResponse = await signIn(userInfo).unwrap();
+        console.log(result);
+        dispatch(addAuthUser({ authInfo: result.user }));
+        dispatch(updateLogout(false));
+        saveToSessionStorage(JSON.stringify(true), JSON.stringify(result.user?.username || ''));
+        setAlertMessage('');
+      }
+    } catch (error) {
+      setAlertMessage(error?.data?.message || 'An error occurred while registering user');
+      console.error(error);
+    }
+  };
+
   return (
     <ModalBg>
       <div className="relative top-[20%] mx-auto w-11/12 max-w-md rounded-lg bg-white md:w-2/3">
@@ -20,7 +58,7 @@ const Login: FC<IModalBgProps> = ({ onClose, onToggle, onTogglePassword }): Reac
               label={<FaTimes className="icon icon-tabler icon-tabler-x" onClick={() => onClose?.()} />}
             />
           </div>
-          <Alert type="error" message="" />
+          {alertMessage && <Alert type="error" message={alertMessage} />}
           <div>
             <label htmlFor="email or username" className="text-sm font-bold leading-tight tracking-normal text-gray-800">
               Email or username
@@ -29,7 +67,8 @@ const Login: FC<IModalBgProps> = ({ onClose, onToggle, onTogglePassword }): Reac
               id="username"
               name="username"
               type="text"
-              value=""
+              value={userInfo.username}
+              onChange={(event: ChangeEvent) => setUserInfo({ ...userInfo, username: (event.target as HTMLInputElement).value })}
               className="mb-5 mt-2 flex h-10 w-full items-center rounded border border-gray-300 pl-3 text-sm font-normal text-gray-600 focus:border focus:border-sky-500/50 focus:outline-none"
               placeholder="Enter username"
             />
@@ -40,13 +79,18 @@ const Login: FC<IModalBgProps> = ({ onClose, onToggle, onTogglePassword }): Reac
             </label>
             <div className="relative mb-2 mt-2">
               <div className="absolute right-0 flex h-full cursor-pointer items-center pr-3 text-gray-600">
-                <FaEye />
+                {passwordType === 'password' ? (
+                  <FaEyeSlash onClick={() => setPasswordType('text')} className="icon icon-tabler icon-tabler-info-circle" />
+                ) : (
+                  <FaEye onClick={() => setPasswordType('password')} className="icon icon-tabler icon-tabler-info-circle" />
+                )}
               </div>
               <TextInput
                 id="password"
                 name="password"
                 type="password"
-                value=""
+                value={userInfo.password}
+                onChange={(event: ChangeEvent) => setUserInfo({ ...userInfo, password: (event.target as HTMLInputElement).value })}
                 className="flex h-10 w-full items-center rounded border border-gray-300 pl-3 text-sm font-normal text-gray-600 focus:border focus:border-sky-500/50 focus:outline-none"
                 placeholder="Enter password"
               />
@@ -58,9 +102,15 @@ const Login: FC<IModalBgProps> = ({ onClose, onToggle, onTogglePassword }): Reac
           <div className="flex w-full items-center justify-center">
             <Button
               testId="submit"
-              disabled={false}
-              className="text-md block w-full cursor-pointer rounded bg-sky-500 px-8 py-2 text-center font-bold text-white hover:bg-sky-400 focus:outline-none"
-              label="LOGIN"
+              disabled={!userInfo.username || !userInfo.password}
+              onClick={() => onLoginUser()}
+              className={cn(
+                'text-md block w-full cursor-pointer rounded bg-sky-500 px-8 py-2 text-center font-bold text-white hover:bg-sky-400 focus:outline-none',
+                {
+                  'opacity-50 cursor-not-allowed': !userInfo.username || !userInfo.password || isLoading
+                }
+              )}
+              label={isLoading ? 'LOGIN IN PROGRESS...' : 'LOGIN'}
             />
           </div>
         </div>
