40924f223df12526bcdf0e260c709e715b9c0f87
mjoe <colakkemre@gmail.com>
2026-02-26 13:56:02 +0300

    feat(AppPage): enhance user authentication flow with session storage management and conditional rendering

--- client/src/features/AppPage.tsx
+++ client/src/features/AppPage.tsx
@@ -1,8 +1,12 @@
-import { FC, ReactElement, useState } from 'react';
+import { FC, ReactElement, useCallback, useEffect, useState } from 'react';
 import Index from './index/Index';
-import { useAppSelector } from 'src/store/store';
+import { useAppDispatch, useAppSelector } from 'src/store/store';
 import { IReduxState } from 'src/store/store.interface';
 import { useCheckCurrentUserQuery } from './auth/services/auth.service';
+import { addAuthUser } from './auth/reducers/auth.reducer';
+import { saveToSessionStorage } from 'src/shared/utils/utils.service';
+import Home from './home/Home';
+import HomeHeader from 'src/shared/header/components/HomeHeader';
 
 const AppPage: FC = (): ReactElement => {
   const authUser = useAppSelector((state: IReduxState) => state.authUser);
@@ -10,14 +14,39 @@ const AppPage: FC = (): ReactElement => {
   const showCategoryContainer = true;
   const [tokenIsValid, setTokenIsValid] = useState<boolean>(false);
 
+  const dispatch = useAppDispatch();
   const { data, isError, isSuccess, isLoading } = useCheckCurrentUserQuery();
-  console.log(data, 'data', isError, 'isError', isSuccess, 'isSuccess', isLoading, 'isLoading');
 
-  return (
-    <div>
+  const checkUser = useCallback(async () => {
+    try {
+      if (data && data.user && !appLogout) {
+        setTokenIsValid(true);
+        dispatch(addAuthUser({ authInfo: data.user }));
+        // dispatch buyer info
+        // dispatch seller info
+        saveToSessionStorage(JSON.stringify(true), JSON.stringify(authUser.username));
+      }
+    } catch (error) {
+      console.log(error, 'error');
+    }
+  }, [data, dispatch, appLogout, authUser.username]);
+
+  useEffect(() => {
+    checkUser();
+  }, [checkUser]);
+
+  if (authUser) {
+    return !tokenIsValid && !authUser.id ? (
       <Index />
-    </div>
-  );
+    ) : (
+      <>
+        <HomeHeader />
+        <Home />
+      </>
+    );
+  } else {
+    return <Index />;
+  }
 };
 
 export default AppPage;
