23083e351d02cb5192858c8547d3efc5c051aeda
mjoe <colakkemre@gmail.com>
2026-02-26 16:13:43 +0300

    feat(Home): add HomeGigsView component to display top gigs on the platform

--- client/src/features/home/Home.tsx
+++ client/src/features/home/Home.tsx
@@ -1,10 +1,17 @@
 import { FC, ReactElement } from 'react';
 import HomeSlider from './components/HomeSlider';
+import HomeGigsView from './components/HomeGigsView';
 
 const Home: FC = (): ReactElement => {
   return (
     <div className="m-auto px-6 w-screen relative min-h-screen xl:container md:px-12 lg:px-6">
       <HomeSlider />
+      <HomeGigsView
+        gigs={[]}
+        title="Because you view gig on"
+        subTitle="These are the top gigs on the platform"
+        category="Programming & Tech"
+      />
     </div>
   );
 };
