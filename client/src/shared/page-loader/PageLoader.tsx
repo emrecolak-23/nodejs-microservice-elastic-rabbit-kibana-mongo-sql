d2875153b341b44b2a4a1b54c0e883c00cc2d657
mjoe <colakkemre@gmail.com>
2026-02-27 14:46:50 +0300

    fix(jobber-share): update subproject commit to indicate dirty state

---
+++ client/src/shared/page-loader/PageLoader.tsx
@@ -0,0 +1,7 @@
+import { FC, ReactElement } from 'react';
+
+const PageLoader: FC = (): ReactElement => {
+  return <div className="w-screen h-screen bg-[#eef2f6]"></div>;
+};
+
+export default PageLoader;
