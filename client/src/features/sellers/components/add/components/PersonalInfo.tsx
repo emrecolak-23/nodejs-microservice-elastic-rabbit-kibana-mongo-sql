439919302d345c62d395499a2321b08b0033a3d9
mjoe <colakkemre@gmail.com>
2026-02-27 16:16:08 +0300

    feat(PersonalInfo): enhance input handling with character limits and dynamic feedback for oneliner and description fields

--- client/src/features/sellers/components/add/components/PersonalInfo.tsx
+++ client/src/features/sellers/components/add/components/PersonalInfo.tsx
@@ -1,9 +1,17 @@
-import { FC, ReactElement } from 'react';
+import { ChangeEvent, FC, KeyboardEvent, ReactElement, useState } from 'react';
 import { IPersonalInfoProps } from 'src/features/sellers/interfaces/seller.interface';
 import TextInput from 'src/shared/inputs/TextInput';
 import TextAreaInput from 'src/shared/inputs/TextAreaInput';
 
 const PersonalInfo: FC<IPersonalInfoProps> = ({ personalInfo, setPersonalInfo }): ReactElement => {
+  const [allowedInfoLength, setAllowedInfoLength] = useState<{ oneliner: string; description: string }>({
+    oneliner: '70/70',
+    description: '600/600'
+  });
+
+  const maxDescriptionCharacters = 600;
+  const maxOneLinerCharacters = 70;
+
   return (
     <div className="border-b border-grey p-6">
       <div className="mb-6 grid md:grid-cols-5">
@@ -16,7 +24,9 @@ const PersonalInfo: FC<IPersonalInfoProps> = ({ personalInfo, setPersonalInfo })
             type="text"
             name="fullname"
             value={personalInfo.fullName}
-            onChange={(e) => setPersonalInfo({ ...personalInfo, fullName: (e.target as HTMLInputElement).value })}
+            onChange={(e: ChangeEvent) => {
+              setPersonalInfo({ ...personalInfo, fullName: (e.target as HTMLInputElement).value });
+            }}
           />
         </div>
       </div>
@@ -31,10 +41,22 @@ const PersonalInfo: FC<IPersonalInfoProps> = ({ personalInfo, setPersonalInfo })
             rows={5}
             name="oneliner"
             value={personalInfo.oneliner}
-            onChange={(e) => setPersonalInfo({ ...personalInfo, oneliner: (e.target as HTMLInputElement).value })}
+            onChange={(event: ChangeEvent) => {
+              const rawValue: string = (event.target as HTMLInputElement).value;
+              const onelinerValue: string = rawValue.slice(0, maxOneLinerCharacters);
+              setPersonalInfo({ ...personalInfo, oneliner: onelinerValue });
+              const counter: number = maxOneLinerCharacters - onelinerValue.length;
+              setAllowedInfoLength({ ...allowedInfoLength, oneliner: `${counter}/70` });
+            }}
+            onKeyDown={(event: KeyboardEvent<Element>) => {
+              const currentTextLength: number = (event.target as HTMLInputElement).value.length;
+              if (currentTextLength === maxOneLinerCharacters && event.key !== 'Backspace') {
+                event.preventDefault();
+              }
+            }}
             placeholder="E.g. Expert Mobile and Web Developer"
           />
-          <span className="flex justify-end text-[#95979d] text-xs">20 Characters</span>
+          <span className="flex justify-end text-[#95979d] text-xs">{allowedInfoLength.oneliner} Characters</span>
         </div>
       </div>
       <div className="grid md:grid-cols-5 mb-6">
@@ -46,10 +68,22 @@ const PersonalInfo: FC<IPersonalInfoProps> = ({ personalInfo, setPersonalInfo })
             className="w-full rounded border border-grey p-2.5 mb-1 text-sm font-normal text-gray-600 focus:outline-none"
             name="description"
             value={personalInfo.description}
-            onChange={(e) => setPersonalInfo({ ...personalInfo, description: (e.target as HTMLTextAreaElement).value })}
+            onChange={(event: ChangeEvent) => {
+              const rawValue: string = (event.target as HTMLInputElement).value;
+              const descriptionValue: string = rawValue.slice(0, maxDescriptionCharacters);
+              setPersonalInfo({ ...personalInfo, description: descriptionValue });
+              const counter: number = maxDescriptionCharacters - descriptionValue.length;
+              setAllowedInfoLength({ ...allowedInfoLength, description: `${counter}/600` });
+            }}
+            onKeyDown={(event: KeyboardEvent<Element>) => {
+              const currentTextLength: number = (event.target as HTMLTextAreaElement).value.length;
+              if (currentTextLength === maxDescriptionCharacters && event.key !== 'Backspace') {
+                event.preventDefault();
+              }
+            }}
             rows={5}
           />
-          <span className="flex justify-end text-[#95979d] text-xs">100 Characters</span>
+          <span className="flex justify-end text-[#95979d] text-xs">{allowedInfoLength.description} Characters</span>
         </div>
       </div>
       <div className="grid md:grid-cols-5 mb-6">
@@ -62,7 +96,10 @@ const PersonalInfo: FC<IPersonalInfoProps> = ({ personalInfo, setPersonalInfo })
             type="number"
             name="responseTime"
             value={personalInfo.responseTime}
-            onChange={(e) => setPersonalInfo({ ...personalInfo, responseTime: (e.target as HTMLInputElement).value })}
+            onChange={(e: ChangeEvent) => {
+              const value: string = (e.target as HTMLInputElement).value;
+              setPersonalInfo({ ...personalInfo, responseTime: parseInt(value) > 0 ? value : '' });
+            }}
             placeholder="E.g. 1"
           />
         </div>
