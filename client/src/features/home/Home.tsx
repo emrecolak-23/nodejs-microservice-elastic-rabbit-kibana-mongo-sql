d2f58e8102dabba050e537f243eeee3df31d802e
mjoe <colakkemre@gmail.com>
2026-03-01 14:21:17 +0300

    feat(Home, FeatureExperts): integrate random sellers fetching and enhance seller display with dynamic data; update seller profile links and ratings presentation

--- client/src/features/home/Home.tsx
+++ client/src/features/home/Home.tsx
@@ -2,8 +2,18 @@ import { FC, ReactElement } from 'react';
 import HomeSlider from './components/HomeSlider';
 import HomeGigsView from './components/HomeGigsView';
 import FeatureExperts from './components/FeatureExperts';
+import { useGetRandomSellersQuery } from '../sellers/services/seller.service';
+import { ISellerDocument } from '../sellers/interfaces/seller.interface';
 
 const Home: FC = (): ReactElement => {
+  const { data, isSuccess } = useGetRandomSellersQuery('10');
+
+  let sellers: ISellerDocument[] = [];
+
+  if (isSuccess) {
+    sellers = data?.sellers as ISellerDocument[];
+  }
+
   return (
     <div className="m-auto px-6 w-screen relative min-h-screen xl:container md:px-12 lg:px-6">
       <HomeSlider />
--- client/src/features/home/components/FeatureExperts.tsx
+++ client/src/features/home/components/FeatureExperts.tsx
@@ -2,9 +2,10 @@ import { FC, ReactElement } from 'react';
 import { IFeaturedExpertProps } from '../interfaces/home.interface';
 import { Link } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
-import { lowerCase } from 'src/shared/utils/utils.service';
+import { lowerCase, rating } from 'src/shared/utils/utils.service';
 import { FaRegStar } from 'react-icons/fa';
 import StarRating from 'src/shared/rating/StarRating';
+import { ISellerDocument } from 'src/features/sellers/interfaces/seller.interface';
 
 const FeatureExperts: FC<IFeaturedExpertProps> = ({ sellers }): ReactElement => {
   return (
@@ -15,27 +16,35 @@ const FeatureExperts: FC<IFeaturedExpertProps> = ({ sellers }): ReactElement => 
       </div>
       <div className="mt-6">
         <div className="grid gap-8 pt-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {[...sellers, 1, 2, 3, 4, 5, 6, 7, 8].map(() => (
+          {[...sellers].map((seller: ISellerDocument) => (
             <div key={uuidv4()} className="w-full rounded-lg border border-grey bg-white shadow">
               <div className="flex flex-col items-center pb-10 pt-5">
                 <img
                   className="mb-3 h-24 w-24 rounded-full shadow-lg"
-                  src="https://placehold.co/330x220?text=Profile+Image"
+                  src={seller.profilePicture || 'https://placehold.co/330x220?text=Profile+Image'}
                   alt="Profile image"
                 />
-                <h5 className="mb-1 xl:text-xl font-medium text-gray-900 ">Danny</h5>
-                <span className="text-sm w-[90%] mb-1 text-gray-500 text-center dark:text-gray-500">This is what i do</span>
+                <h5 className="mb-1 xl:text-xl font-medium text-gray-900 ">{seller.username}</h5>
+                <span className="text-sm w-[90%] mb-1 text-gray-500 text-center dark:text-gray-500">{seller.oneliner}</span>
                 <div className="flex justify-center w-full gap-x-1 self-center h-6">
                   <div className="mt-1 w-20 gap-x-2 flex">
-                    <StarRating value={4} size={14} setReviewRating={() => {}} />
+                    <StarRating
+                      value={rating(parseInt(`${seller?.ratingSum}`) / parseInt(`${seller?.ratingsCount}`))}
+                      size={14}
+                      setReviewRating={() => {}}
+                    />
                   </div>
-                  <div className="ml-2 flex self-center gap-1 rounded bg-orange-400 px-1 text-xs">
-                    <span className="font-bold text-white">5</span>
-                  </div>
+                  {parseInt(`${seller?.ratingsCount}`) > 0 && (
+                    <div className="ml-2 flex self-center gap-1 rounded bg-orange-400 px-1 text-xs">
+                      <span className="font-bold text-white">
+                        {rating(parseInt(`${seller?.ratingSum}`) / parseInt(`${seller?.ratingsCount}`))}
+                      </span>
+                    </div>
+                  )}
                 </div>
                 <div className="mt-4 flex space-x-3 md:mt-6">
                   <Link
-                    to={`/seller-profile/${lowerCase('danny')}/1213141441/view`}
+                    to={`/seller-profile/${lowerCase(`${lowerCase(`${seller?.username}`)}`)}/${seller?._id}/view`}
                     className="rounded bg-sky-500 px-6 py-3 text-center text-sm font-bold text-white hover:bg-sky-400 focus:outline-none md:px-4 md:py-2 md:text-base"
                   >
                     View Profile
