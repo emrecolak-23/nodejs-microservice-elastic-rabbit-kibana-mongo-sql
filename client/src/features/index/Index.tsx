f9a2c18662dbacba3df0845acd7220bd5834c30b
mjoe <colakkemre@gmail.com>
2026-03-01 14:22:52 +0300

    feat(Index): add session storage management on component mount to enhance user state handling

--- client/src/features/index/Index.tsx
+++ client/src/features/index/Index.tsx
@@ -1,17 +1,23 @@
-import { FC, LazyExoticComponent, ReactElement, Suspense, lazy } from 'react';
+import { FC, LazyExoticComponent, ReactElement, Suspense, lazy, useEffect } from 'react';
 import { IHeader } from 'src/shared/header/interfaces/header.interface';
 
 import GigTabs from './gig-tabs/GigTabs';
 import Hero from './Hero';
 import HowItWorks from './HowItWorks';
 import Categories from './Categories';
+import { saveToSessionStorage } from 'src/shared/utils/utils.service';
+import CircularPageLoader from 'src/shared/page-loader/CircularPageLoader';
 
 const IndexHeader: LazyExoticComponent<FC<IHeader>> = lazy(() => import('../../shared/header/components/Header'));
 
 const Index: FC = (): ReactElement => {
+  useEffect(() => {
+    saveToSessionStorage(JSON.stringify(false), JSON.stringify(''));
+  }, []);
+
   return (
     <div className="flex flex-col">
-      <Suspense>
+      <Suspense fallback={<CircularPageLoader />}>
         <IndexHeader navClass="navbar peer-checked:navbar-active fixed z-20 w-full border-b border-gray-100 bg-white shadow-2xl shadow-gray-600/5 dark:border-gray-800 dark:bg-gray-900 dark:shadow-none" />
         <Hero />
         <GigTabs />
